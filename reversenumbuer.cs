8c406fc15a05e6c0bddfa5a9ac7f32e2b4f42c1c
poojakhandale <poojakhandale2000@gmail.com>
2023-02-14 23:08:04 +0530

    reverse

---
+++ reversenumbuer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalprogramming.fibonacci
+{
+    internal class reversenumbuer
+    {
+        public static void  Reverse()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter the number");
+            n=Convert.ToInt32(Console.ReadLine());
+            while (n!=0)
+            {
+                rem = n % 10;
+                reverse=reverse * 10 + rem;
+                n=n/10;
+          
+            }
+            Console.WriteLine("Reverse Number" + reverse);
+        }
+    }
+}
